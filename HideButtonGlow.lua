1c320a2c05710f3281f1ddd90d515857fdc3ca56
Nick <ntowle@gmail.com>
2022-10-05 12:58:54 -0400

    perf improvement

--- HideButtonGlow.lua
+++ HideButtonGlow.lua
@@ -22,37 +22,41 @@ function addonFrame:PLAYER_LOGIN()
     end
 end
 
-function addon:addMessage(message, debugOnly)
-    if not debugOnly or HideButtonGlowDB.debugMode then
-        DEFAULT_CHAT_FRAME:AddMessage(message)
-    end
+function addon:addMessage(message)
+    DEFAULT_CHAT_FRAME:AddMessage(message)
 end
 
 function addon:shouldHideGlow(spellId)
-    local spellName = GetSpellInfo(spellId)
-
     -- check if the 'hide all' option is set
     if HideButtonGlowDB.hideAll then
         for _, spellToAllow in ipairs(HideButtonGlowDB.allowedSpells) do
             if spellId == spellToAllow then
-                addon:addMessage(("Found in allow list, allowing spell glow for %s (ID %d)."):format(name, spellId), true)
+                if HideButtonGlowDB.debugMode then
+                    addon:addMessage(("Found in allow list, allowing spell glow for %s (ID %d)."):format(GetSpellInfo(spellId), spellId))
+                end
                 return false
             end
         end
-        addon:addMessage(("Hide All is checked, hiding spell glow for %s (ID %d)."):format(name, spellId), true)
+        if HideButtonGlowDB.debugMode then
+            addon:addMessage(("Hide All is checked, hiding spell glow for %s (ID %d)."):format(GetSpellInfo(spellId), spellId))
+        end
         return true
     end
 
     -- else iterate through filter list
     for _, spellToFilter in ipairs(HideButtonGlowDB.spells) do
         if spellId == spellToFilter then
-            addon:addMessage(("Filter matched, hiding spell glow for %s (ID %d)."):format(name, spellId), true)
+            if HideButtonGlowDB.debugMode then
+                addon:addMessage(("Filter matched, hiding spell glow for %s (ID %d)."):format(GetSpellInfo(spellId), spellId))
+            end
             return true
         end
     end
 
     -- else show the glow
-    addon:addMessage(("No filters matched, allowing spell glow for %s (ID %d)."):format(name, spellId), true)
+    if HideButtonGlowDB.debugMode then
+        addon:addMessage(("No filters matched, allowing spell glow for %s (ID %d)."):format(GetSpellInfo(spellId), spellId))
+    end
     return false
 end
 
